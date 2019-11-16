c0fd548d2594f4287d66ef875af227e6c2356a2b
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-16 19:43:17 +0100

    made PORT dynamic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express()
+const port = process.env.PORT || 3000
 const userRouter = require("./api/useRoutes")
 const authroutes = require("./api/authRoutes")
 
@@ -14,4 +15,4 @@ app.use('/register', authroutes)
 
 
 
-app.listen(3000, () => console.log("server listening on port 3000"))
+app.listen(port, () => console.log("server listening on port "))
