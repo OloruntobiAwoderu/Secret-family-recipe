0bc01b8d897ac414537651643f8449524eb54e40
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-16 13:19:09 +0100

    Seeded data into the databse

---
+++ data/migrations/20191116122332_users_table.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("recipes", function(recipes) {
+    recipes.increments();
+    recipes
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    recipes.string("title", 255).notNullable();
+    recipes.string("source", 255).notNullable();
+    recipes.text("ingredients").notNullable();
+    recipes.text("instructions").notNullable();
+    recipes.string("category", 255).notNullable();
+    recipes.binary("image");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("recipes");
+};
---
+++ data/seeds/users_seed.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex) {
+  return knex("users").insert([
+    {
+      id: 1,
+      name: "Oloruntobi",
+      email: "tobi@gmail.com",
+      password: "florence"
+    },
+    {
+      id: 2,
+      name: "awoderu",
+      email: "tobi1@gmail.com",
+      password: "florence"
+    },
+    {
+      id: 3,
+      name: "messi",
+      email: "tobi2@gmail.com",
+      password: "florence"
+    },
+    {
+      id: 4,
+      name: "ronaldo",
+      email: "tobi3@gmail.com",
+      password: "florence"
+    }
+  ]);
+};
